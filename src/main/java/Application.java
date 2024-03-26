0da161dea097a8bec2004ae6966a8062cabf434a
Anton Romanovsky <romcky@gmail.com>
2024-03-26 17:26:41 +0300

    try with resources

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,25 +8,24 @@ public class Application {
         var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
 
         var sql = "create table users (id bigint primary key auto_increment, username varchar(255), phone varchar(255));";
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
+        try (var statement = conn.createStatement()) {
+            statement.execute(sql);
+        }
 
         var sql2 = "insert into users (username, phone) values ('tommy', '123456789'), ('bobby', '987654321');";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
+        try (var statement2 = conn.createStatement()) {
+            statement2.executeUpdate(sql2);
+        }
 
         var sql3 = "select * from users;";
-        var statement3 = conn.createStatement();
-        var resultSet = statement3.executeQuery(sql3);
-        while (resultSet.next()) {
-            System.out.println("username: " + resultSet.getString("username"));
-            System.out.println("phone: " + resultSet.getString("phone"));
-            System.out.println();
+        try (var statement3 = conn.createStatement()) {
+            var resultSet = statement3.executeQuery(sql3);
+            while (resultSet.next()) {
+                System.out.println("username: " + resultSet.getString("username"));
+                System.out.println("phone: " + resultSet.getString("phone"));
+                System.out.println();
+            }
         }
-        statement3.close();
-
         conn.close();
     }
 }
